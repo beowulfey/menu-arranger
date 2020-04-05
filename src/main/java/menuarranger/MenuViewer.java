53ea18b4c4ab890edd149e3fa859f67a4498d759
beowulfey <wolfe.aarond@gmail.com>
2020-04-05 07:29:25 -0400

    Made a mini status bar in my dialog to help users if they eff it up.

--- src/main/java/menuarranger/MenuViewer.java
+++ src/main/java/menuarranger/MenuViewer.java
@@ -9,6 +9,8 @@ import javax.swing.tree.TreeSelectionModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 public class MenuViewer extends JDialog {
     protected JPanel rootPanel;
@@ -24,10 +26,8 @@ public class MenuViewer extends JDialog {
     protected JTree customTree;
     protected JButton addFolderButton;
     protected JPanel topPanel;
-    protected JTextField newNode;
-    protected JButton button2;
-    protected JButton button3;
-    protected JButton button4;
+    protected JTextField nodeName;
+    protected JTextField statusField;
 
     private DefaultMutableTreeNode newRoot = new DefaultMutableTreeNode("Drop Here", true);
     private DefaultTreeModel newTreeModel = new DefaultTreeModel(newRoot);
@@ -40,42 +40,63 @@ public class MenuViewer extends JDialog {
         menuTreeModel = new DefaultTreeModel(rootCopy);
         menuTree.setModel(menuTreeModel);
         customTree.setModel(newTreeModel);
+        statusField.setForeground(new Color(250, 0, 0));
+        statusField.setText("");
 
         // I don't want to have to deal with moving multiple nodes, sorry!
         menuTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
-        customTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
-
-        // Setup button actions
-        //refreshButton.addActionListener(e -> {
-        //    menuTree.setModel(menuTreeModel);
-        //    repaint();
-        //});
+        customTree.getSelectionModel().setSelectionMode(TreeSelectionModel.DISCONTIGUOUS_TREE_SELECTION);
 
         OKButton.addActionListener(e -> onOK());
+
+        nodeName.addFocusListener(new FocusListener() {
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (nodeName.getText().trim().equals("")) {
+                    //nodeName.setEnabled(false);
+                    nodeName.setText("New Folder Name");
+
+                }
+            }
+
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (nodeName.getText().trim().equals("New Folder Name")) {
+                    nodeName.setText("");
+                }
+            }
+        });
+
         cancelButton.addActionListener(e -> onCancel());
         deleteButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // NEED TO FIGURE OUT HOW TO MAKE THIS WORK SO IT DOESN'T DELETE THE BLANK NODE IF EMPTY!
-                // ALSO NEED TO MAKE IT SO I CAN'T DO ON THE ROOT NODE!
-                //DefaultMutableTreeNode temp = new DefaultMutableTreeNode(customTree.getLastSelectedPathComponent());
-                //if (newTreeModel.getChildCount(temp.getParent()) < 1 && customTree.getLastSelectedPathComponent().toString() != "") {
-                newTreeModel.removeNodeFromParent((DefaultMutableTreeNode) customTree.getLastSelectedPathComponent());
-                //System.out.println("New Tree " + newRoot.getChildAt(0).getChildCount());
-                //System.out.println("Old tree " + menuRoot.getChildAt(0).getChildCount());
-                //}
-                //else {
-                //    System.out.println("CAN'T DELETE THAT!");
-                //}
+                // Can I get this to delete multiple nodes at once? Low priority.
+                try {
+                    newTreeModel.removeNodeFromParent((DefaultMutableTreeNode) customTree.getLastSelectedPathComponent());
+                } catch (IllegalArgumentException err) {
+                    statusField.setText("Can't delete the root node!");
+                } catch (NullPointerException err) {
+                    statusField.setText("Please select a site first!");
+                }
             }
         });
         addFolderButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                DefaultMutableTreeNode last = (DefaultMutableTreeNode) customTree.getLastSelectedPathComponent();
-                DefaultMutableTreeNode insertNode = new DefaultMutableTreeNode("New Folder", true);
-                // In order to get around my restriction in the canImport function below (can't drop on leaves)...
-                //DefaultMutableTreeNode fakeNode = new DefaultMutableTreeNode("", false);
-                //insertNode.add(fakeNode);
-                newTreeModel.insertNodeInto(insertNode, last, last.getChildCount());
+                try {
+                    DefaultMutableTreeNode last = (DefaultMutableTreeNode) customTree.getLastSelectedPathComponent();
+                    TreePath path = customTree.getSelectionPath();
+                    DefaultMutableTreeNode insertNode = new DefaultMutableTreeNode(nodeName.getText(), true);
+                    newTreeModel.insertNodeInto(insertNode, last, last.getChildCount());
+                    customTree.makeVisible(path.pathByAddingChild(insertNode));
+                    customTree.scrollRectToVisible(customTree.getPathBounds(path.pathByAddingChild(insertNode)));
+                    nodeName.setText("New Folder Name");
+
+                } catch (NullPointerException err) {
+                    statusField.setForeground(new Color(250, 0, 0));
+                    statusField.setText("Please select a site first!");
+
+
+                }
             }
         });
 
@@ -198,16 +219,19 @@ public class MenuViewer extends JDialog {
         rootPanel.setPreferredSize(new Dimension(600, 350));
         rootPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLoweredBevelBorder(), null));
         bottomPanel = new JPanel();
-        bottomPanel.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(1, 3, new Insets(0, 0, 0, 0), -1, -1));
+        bottomPanel.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(1, 4, new Insets(0, 0, 0, 0), -1, -1));
         rootPanel.add(bottomPanel, new com.intellij.uiDesigner.core.GridConstraints(2, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         cancelButton = new JButton();
         cancelButton.setText("Cancel");
-        bottomPanel.add(cancelButton, new com.intellij.uiDesigner.core.GridConstraints(0, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_EAST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(100, -1), null, 0, false));
+        bottomPanel.add(cancelButton, new com.intellij.uiDesigner.core.GridConstraints(0, 3, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_EAST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(100, -1), null, 0, false));
         OKButton = new JButton();
         OKButton.setText("OK");
-        bottomPanel.add(OKButton, new com.intellij.uiDesigner.core.GridConstraints(0, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(100, -1), null, 0, false));
+        bottomPanel.add(OKButton, new com.intellij.uiDesigner.core.GridConstraints(0, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(100, -1), null, 0, false));
+        statusField = new JTextField();
+        statusField.setOpaque(false);
+        bottomPanel.add(statusField, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(200, -1), null, 0, false));
         final com.intellij.uiDesigner.core.Spacer spacer1 = new com.intellij.uiDesigner.core.Spacer();
-        bottomPanel.add(spacer1, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, new Dimension(14, 30), null, 0, false));
+        bottomPanel.add(spacer1, new com.intellij.uiDesigner.core.GridConstraints(0, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         topPanel = new JPanel();
         topPanel.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(2, 4, new Insets(0, 0, 0, 0), -1, -1));
         rootPanel.add(topPanel, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(258, 469), null, 0, false));
@@ -249,9 +273,10 @@ public class MenuViewer extends JDialog {
         addFolderButton = new JButton();
         addFolderButton.setText("Add");
         topPanel.add(addFolderButton, new com.intellij.uiDesigner.core.GridConstraints(1, 2, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_EAST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(84, -1), null, 0, false));
-        newNode = new JTextField();
-        newNode.setToolTipText("Enter name for new node");
-        topPanel.add(newNode, new com.intellij.uiDesigner.core.GridConstraints(1, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_EAST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        nodeName = new JTextField();
+        nodeName.setText("New Folder Name");
+        nodeName.setToolTipText("Enter name for new node");
+        topPanel.add(nodeName, new com.intellij.uiDesigner.core.GridConstraints(1, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_EAST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
         final JSeparator separator1 = new JSeparator();
         rootPanel.add(separator1, new com.intellij.uiDesigner.core.GridConstraints(1, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(0, 0), null, 0, false));
     }
